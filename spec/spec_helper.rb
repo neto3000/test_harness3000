a67688f39680ab7ce40ef65614f9409a557ce8c8
Ernesto Alcaraz Andrade <ernesto.alcaraz@bigcommerce.com>
2019-05-22 15:27:21 -0500

    load rspec config from spec helper and add buildpack

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -103,9 +103,21 @@ require File.expand_path('../../config/environment', __FILE__)
 
 require 'rspec/rails'
 require 'capybara/rspec'
+#
+# Capybara.register_driver :selenium_chrome do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
 
-Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+
+chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+  )
 end
 
-Capybara.javascript_driver = :selenium_chrome
+Capybara.javascript_driver = :chrome
